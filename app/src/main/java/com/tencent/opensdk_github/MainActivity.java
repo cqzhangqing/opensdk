149bb8410653fe58f9e42f49552d84d45ff5b599
cqzhangqing <714851760@qq.com>
2020-08-29 00:37:25 +0800

    add mylibs

--- app/src/main/java/com/tencent/opensdk_github/MainActivity.java
+++ app/src/main/java/com/tencent/opensdk_github/MainActivity.java
@@ -1,8 +1,10 @@
 package com.tencent.opensdk_github;
 
+import android.view.View;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.tencent.mylibs.TencentImpl;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 Toast.makeText(MainActivity.this, "在主线程执行代码", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        final TencentImpl tencentImpl = new TencentImpl(this, "com.tencent.opensdk_github.fileprovider");
+
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tencentImpl.login(MainActivity.this, "all");
+            }
+        });
+
+        findViewById(R.id.share).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                bundle.putString("uin", "7148517");
+                bundle.putString("title", "title");
+                tencentImpl.share(MainActivity.this, bundle);
             }
         });
     }
