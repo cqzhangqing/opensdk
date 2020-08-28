6efb7e9fed09146af21254ce45388f4b6aa9f981
qingzqzhang <qingzqzhang@tencent.com>
2020-08-28 15:52:14 +0800

    first commit

---
+++ app/src/main/java/com/tencent/opensdk_github/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tencent.opensdk_github;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
